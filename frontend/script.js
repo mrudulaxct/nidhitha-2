62e213db5ef43389b557aa42d48f20d618a34d10
saicharan <108361355+Saicharan750@users.noreply.github.com>
2025-06-19 00:27:02 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://cyber-security-trust-model-main1-1.onrender.com';
+const API_BASE = 'https://cyber-security-trust-model-main1.onrender.com';
 
 let currentEncryptionData = {};
 
